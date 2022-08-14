a013e299f5e3192788691602d71e1e353d855c55
calmgiraffe <hans.park6@gmail.com>
2022-08-14 12:03:00 -0400

    Updated testAbleToMerge

--- proj0/game2048/TestAbleToMerge.java
+++ proj0/game2048/TestAbleToMerge.java
@@ -1,66 +1,42 @@
 package game2048;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class TestAbleToMerge {
 
-    public static boolean ableToMerge(int col, int row, int topRow, Board board) {
-        for (int r = row + 1; r <= topRow; r++) {
-
-            // If null, continue to next iteration
-            if (board.tile(col, r) != null && board.tile(col, r).value() == board.tile(col, row).value()) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static void testMerge1() {
-
+    @Test
+    public void testMerge1() {
         int[][] rawVals = new int[][] {
                 {0, 0, 0, 0},
                 {0, 0, 0, 0},
                 {0, 0, 0, 0},
                 {2, 0, 0, 0},
         };
-
-
-        Board board = new Board(rawVals, 0);
-        boolean output = TestAbleToMerge.ableToMerge(0, 0, 3, board);
-        System.out.println((output));
+        Model m = new Model(rawVals, 0, 0, false);
+        assertFalse(m.canMerge(0, 0, 3));
     }
 
-    public static void testMerge2() {
-
+    @Test
+    public void testMerge2() {
         int[][] rawVals = new int[][] {
                 {4, 0, 0, 0},
                 {0, 0, 0, 0},
                 {0, 0, 0, 0},
                 {2, 0, 0, 0},
         };
-
-
-        Board board = new Board(rawVals, 0);
-        boolean output = TestAbleToMerge.ableToMerge(0, 0, 3, board);
-        System.out.println((output));
+        Model m = new Model(rawVals, 0, 0, false);
+        assertFalse(m.canMerge(0, 0, 3));
     }
 
-    public static void testMerge3() {
-
+    @Test
+    public void testMerge3() {
         int[][] rawVals = new int[][] {
                 {2, 0, 0, 0},
                 {0, 0, 0, 0},
                 {0, 0, 0, 0},
                 {2, 0, 0, 0},
         };
-
-
-        Board board = new Board(rawVals, 0);
-        boolean output = TestAbleToMerge.ableToMerge(0, 0, 3, board);
-        System.out.println((output));
-    }
-
-    public static void main(String[] args) {
-        testMerge1();
-        testMerge2();
-        testMerge3();
+        Model m = new Model(rawVals, 0, 0, false);
+        assertTrue(m.canMerge(0, 0, 3));
     }
 }
