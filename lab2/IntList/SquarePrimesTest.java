5c8ceb518da36414b7dfbe438a164c9f23ae1765
calmgiraffe <hans.park6@gmail.com>
2022-08-16 10:18:55 -0400

    Finished lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,34 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesNoPrimes() {
+        IntList lst = IntList.of(1, 4, 65, 100); // No primes, including the special case of 1
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("1 -> 4 -> 65 -> 100", lst.toString());
+        assertFalse(changed);
+    }
+
+    @Test
+    public void testSquarePrimesAllPrimes() {
+        IntList lst = IntList.of(2, 5, 7, 11, 13, 17); // No primes, including the special case of 1
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 25 -> 49 -> 121 -> 169 -> 289", lst.toString());
+        assertTrue(changed);
+    }
+    @Test
+    public void testSquarePrimes3PrimesEvenIndexed() {
+        IntList lst = IntList.of(2, 6, 7, 12, 13, 18); // No primes, including the special case of 1
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 6 -> 49 -> 12 -> 169 -> 18", lst.toString());
+        assertTrue(changed);
+    }
+    @Test
+    public void testSquarePrimes3PrimesOddIndexed() {
+        IntList lst = IntList.of(4, 5, 6, 7, 12, 13); // No primes, including the special case of 1
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 25 -> 6 -> 49 -> 12 -> 169", lst.toString());
+        assertTrue(changed);
+    }
 }
