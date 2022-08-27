57854090f8600161c8b094f8031078ae7ef7b9f3
calmgiraffe <hans.park6@gmail.com>
2022-08-27 19:42:38 -0400

    done

--- proj1/gh2/TTFAF.java
+++ proj1/gh2/TTFAF.java
@@ -14,6 +14,7 @@ import java.util.zip.GZIPInputStream;
  */
 public class TTFAF {
     public static void main(String[] args) {
+        /*
         try {
             InputStream source = new ByteArrayInputStream(Base64.getDecoder().decode(TTFAF));
             source = new GZIPInputStream(source);
@@ -22,6 +23,10 @@ public class TTFAF {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        */
+
+        GuitarPlayer player = new GuitarPlayer(new java.io.File("C:/Users/hansp/Downloads/pirates.mid"));
+        player.play();
 
         // You can also do this:
         // GuitarPlayer player = new GuitarPlayer(new java.io.File("path/to/music.mid"));
